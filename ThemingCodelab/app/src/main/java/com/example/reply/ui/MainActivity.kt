36384dabab3b1ad3a79bbf57753b0d63612438aa
ElJorgE23 <s21120186@alumnos.itsur.edu.mx>
2024-10-05 13:10:40 -0600

    Lo que se implemeto es que cuando cambiamos el modo oscuro de igual forma cambia en la aplicacion todo esto se trabajo en el MainActivity.kt

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.reply.ui
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
@@ -27,7 +11,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.reply.data.LocalEmailsDataProvider
-
+import com.example.reply.ui.theme.AppTheme // Asegúrate de que esto esté importado
 
 class MainActivity : ComponentActivity() {
 
@@ -37,16 +21,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            // Aquí recolectas el estado del ViewModel
             val uiState by viewModel.uiState.collectAsState()
-            ReplyApp(
-                replyHomeUIState = uiState,
-                closeDetailScreen = {
-                    viewModel.closeDetailScreen()
-                },
-                navigateToDetail = { emailId ->
-                    viewModel.setSelectedEmail(emailId)
-                }
-            )
+
+            // Aplica el tema a la app
+            AppTheme {
+                ReplyApp(
+                    replyHomeUIState = uiState,
+                    closeDetailScreen = {
+                        viewModel.closeDetailScreen()
+                    },
+                    navigateToDetail = { emailId ->
+                        viewModel.setSelectedEmail(emailId)
+                    }
+                )
+            }
         }
     }
 }
@@ -60,10 +49,14 @@ class MainActivity : ComponentActivity() {
     name = "DefaultPreviewLight"
 )
 @Composable
-fun ReplyAppPreviewLight() {
-    ReplyApp(
-        replyHomeUIState = ReplyHomeUIState(
-            emails = LocalEmailsDataProvider.allEmails
+fun ReplyAppPreview() {
+    AppTheme { // Aplica el tema también en las vistas previas
+        ReplyApp(
+            replyHomeUIState = ReplyHomeUIState(
+                emails = LocalEmailsDataProvider.allEmails
+            ),
+            closeDetailScreen = {},
+            navigateToDetail = {}
         )
-    )
+    }
 }
